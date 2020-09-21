d3b0c32b38ab6c107bae306fdb5cbf4434fcd840
Eric P <eprocopio@kubernetes.deloitte.com>
2020-09-21 15:56:06 -0400

    Updated return message

--- flask_web/app.py
+++ flask_web/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Flask is running!"
+    return "Flask is running in GCP!!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
