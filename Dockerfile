2751002f55d5db5698df9af10f9fe03ad5417439
Eric P <eprocopio@kubernetes.deloitte.com>
2020-09-24 17:36:27 -0400

    Added guincorn server

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3.8-slim
 
+ENV PYTHONUNBUFFERED True
+
 COPY ./requirements.txt /app/requirements.txt
 
 WORKDIR /app
@@ -8,8 +10,4 @@ RUN pip install -r requirements.txt
 
 COPY flask_web /app
 
-ENTRYPOINT [ "python" ]
-
-CMD [ "app.py" ]
-
-EXPOSE 8080/tcp
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 main:app
--- flask_web/app.py
+++ flask_web/app.py
@@ -1,12 +1,15 @@
 # flask_web / app.py
 
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Flask is running in GCP!!"
+    name = os.environ.get('NAME', "World")
+    return "Hello {}".format(name)
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
